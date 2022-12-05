011752d6886c2a94bb76ca468c839abcf294621b
Jeremy <jeremy.gross@etu.univ-amu.fr>
2022-12-05 17:25:08 +0100

    avancement de la classe Course

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,20 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Course extends Thread {
     private List<Cheval> chevauxDeCourse = new ArrayList<>();
 
+
     @Override
     public void run() {
-        for (int i = 0; i < 10; ++i) {
-            try {
-                Thread.sleep((long) (Math.random() * 200));
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Bonjour et bienvenue dans ce PMU de diiiingue !!!" + "\n" +
-                    "Vous aller parier sur un chevaux. Tout d'abord, entrer le nombre de chevaux");
-        }
+        Scanner scannerNombreChevaux = new Scanner(System.in);
 
+        System.out.println("Bonjour et bienvenue dans ce PMU de diiiingue !!!" + "\n" +
+                    "Vous aller parier sur un chevaux. Tout d'abord, entrer le nombre de chevaux : " + "\n");
+
+        int nombreChevaux = scannerNombreChevaux.nextInt();
+
+        System.out.println("Vous aller assister à une course de " + nombreChevaux + " chevaux !");
+
+    }
+
+    public static void main(String[] args) {
+        Course course = new Course();
+        course.start();
     }
 }
