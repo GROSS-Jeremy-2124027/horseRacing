3356518c4b565327e2b75e6fbc7381932eb7edd7
Jeremy <jeremy.gross@etu.univ-amu.fr>
2022-12-05 16:23:13 +0100

    premier commit

--- src/main/java/Cheval.java
+++ src/main/java/Cheval.java
@@ -1,2 +1,8 @@
 public class Cheval {
+    private String nomCheval;
+    private int numeroCheval, nombreVictoire;
+    private double coteCheval;
+    private Sexe sexeCheval;
+    private Vitesse vitesse;
+
 }
