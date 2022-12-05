23efbe690b231f6845880e5419ce44cc4ac93983
Jeremy <jeremy.gross@etu.univ-amu.fr>
2022-12-05 16:50:54 +0100

    ajout de nouvelles sous classe de Cheval

---
+++ src/main/java/Mustang.java
@@ -0,0 +1,22 @@
+public class Mustang extends Cheval{
+    private String nomCheval;
+    private int numeroCheval, nombreVictoire;
+    private double coteCheval;
+    private Sexe sexeCheval;
+    private Vitesse vitesse;
+
+
+    public Mustang(String nomCheval, int numeroCheval, int nombreVictoire, double coteCheval, Sexe sexeCheval, Vitesse vitesse) {
+        this.nomCheval = nomCheval;
+        this.numeroCheval = numeroCheval;
+        this.nombreVictoire = nombreVictoire;
+        this.coteCheval = coteCheval;
+        this.sexeCheval = sexeCheval;
+        this.vitesse = vitesse;
+    }
+
+
+    public double calculCote(double coteCheval, double coefficient){
+        return coteCheval * coefficient;
+    }
+}
---
+++ src/main/java/PurSangArabe.java
@@ -0,0 +1,22 @@
+public class PurSangArabe extends Cheval{
+    private String nomCheval;
+    private int numeroCheval, nombreVictoire;
+    private double coteCheval;
+    private Sexe sexeCheval;
+    private Vitesse vitesse;
+
+
+    public PurSangArabe(String nomCheval, int numeroCheval, int nombreVictoire, double coteCheval, Sexe sexeCheval, Vitesse vitesse) {
+        this.nomCheval = nomCheval;
+        this.numeroCheval = numeroCheval;
+        this.nombreVictoire = nombreVictoire;
+        this.coteCheval = coteCheval;
+        this.sexeCheval = sexeCheval;
+        this.vitesse = vitesse;
+    }
+
+
+    public double calculCote(double coteCheval, double coefficient){
+        return coteCheval * coefficient;
+    }
+}
