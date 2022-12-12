48359723dca252b8633a7fc5c5cf1216a0ea0fa9
v21215959 <jules.volpei@etu.univ-amu.fr>
2022-12-12 10:15:25 +0100

    Modification de la classe Course

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Course extends Thread {
     private List<Cheval> chevauxDeCourse = new ArrayList<>();
+    private List<String> typesDeChevaux = new ArrayList<>(List.of("Frisson", "Mustang", "PurSangArabe", "QuarterHorse"));
+    private List<Vitesse> listeDeVitesses = new ArrayList<>(List.of(Vitesse.TRES_LENT, Vitesse.LENT, Vitesse.MOYEN, Vitesse.RAPIDE, Vitesse.TRES_RAPIDE));
+    private List<Sexe> listeDesSexes = new ArrayList<>(List.of(Sexe.MALE, Sexe.FEMALE));
 
 //    @Override
 //    public void run() {
@@ -18,15 +23,79 @@ public class Course extends Thread {
 //
 //    }
 
-    public static void main(String[] args) {
-        String test = GenererNomCheval.donneNomCheval();
-        System.out.println(test);
 
-        QuarterHorseFactory bob = new QuarterHorseFactory(GenererNomCheval.donneNomCheval(), 0, 0, 1.23, Sexe.MALE);
-        Cheval c3 = bob.createChevalMoyen();
-        Cheval cheval1 = Ecurie.createChevalMoyen(new QuarterHorseFactory(GenererNomCheval.donneNomCheval(), 0, 0, 1.23, Sexe.MALE));
-        System.out.println(cheval1);
-        Cheval cheval2 = Ecurie.createChevalRapide(new FrissonFactory(test, 1, 5, 1.002, Sexe.MALE));
-        System.out.println(cheval2);
+    public Course(int nombreChevaux, double coteDeBase) {
+        int compteurCheval = 1;
+        for (int i = 0; i < nombreChevaux; i += 1) {
+            String typeCheval = typesDeChevaux.get(new Random().nextInt(typesDeChevaux.size() - 1));
+            switch (typeCheval) {
+                case "Frisson":
+                    Vitesse vitesse = listeDeVitesses.get(new Random().nextInt(listeDeVitesses.size() - 1));
+                    switch (vitesse) {
+                        case LENT:
+                            Sexe sexe = listeDesSexes.get(new Random().nextInt(listeDesSexes.size() - 1));
+                            switch (sexe) {
+                                case MALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalLent(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.MALE)));
+                                case FEMALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalLent(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.FEMALE)));
+                            }
+                        case TRES_LENT:
+                            sexe = listeDesSexes.get(new Random().nextInt(listeDesSexes.size() - 1));
+                            switch (sexe) {
+                                case MALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalTresLent(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.MALE)));
+                                case FEMALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalTresLent(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.FEMALE)));
+                            }
+                        case MOYEN:
+                            sexe = listeDesSexes.get(new Random().nextInt(listeDesSexes.size() - 1));
+                            switch (sexe) {
+                                case MALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalMoyen(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.MALE)));
+                                case FEMALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalMoyen(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.FEMALE)));
+                            }
+                        case RAPIDE:
+                            sexe = listeDesSexes.get(new Random().nextInt(listeDesSexes.size() - 1));
+                            switch (sexe) {
+                                case MALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalRapide(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.MALE)));
+                                case FEMALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalRapide(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.FEMALE)));
+                            }
+                        case TRES_RAPIDE:
+                            sexe = listeDesSexes.get(new Random().nextInt(listeDesSexes.size() - 1));
+                            switch (sexe) {
+                                case MALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalTresRapide(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.MALE)));
+                                case FEMALE:
+                                    chevauxDeCourse.add(Ecurie.createChevalTresRapide(new FrissonFactory(GenererNomCheval.donneNomCheval(), compteurCheval, 0, coteDeBase, Sexe.FEMALE)));
+                            }
+                    }
+            }
+        }
+    }
+
+    public List<Cheval> getChevauxDeCourse() {
+        return chevauxDeCourse;
+    }
+
+    public List<String> getTypesDeChevaux() {
+        return typesDeChevaux;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Combien voulez-vous de cheval dans la course (Entre 5 et 10 chevaux) : ");
+        Scanner input = new Scanner(System.in);
+        int nombreDeChevaux = input.nextInt();
+        while (nombreDeChevaux < 5 || nombreDeChevaux > 10) {
+            System.out.println("Combien voulez-vous de cheval dans la course (Entre 5 et 10 chevaux) : ");
+            input = new Scanner(System.in);
+            nombreDeChevaux = input.nextInt();
+        }
+        for (int i = 0; i < nombreDeChevaux; i += 1) {
+            String prochainType = this.getTypesDeChevaux().get(new Random().nextInt(getTypesDeChevaux().size()));
+        }
     }
 }
